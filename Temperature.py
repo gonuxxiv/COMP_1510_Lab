d1957e0566d8e8a0fc63d21fe904bd317bf366d1
Geun Uoo Kim <gonukimxxiv@gmail.com>
2020-01-15 13:54:32 -0800

    initial commit

--- Temperature.py
+++
@@ -1,3 +0,0 @@
-celcius = 10
-fahrenheit = (celcius * 9 / 5) + 32
-print(str(celcius) + " Celcius degree =", str(fahrenheit) + " Fahrenheit degree")
--- poem.py
+++
@@ -1,1 +0,0 @@
-print("Roses are red\nViolets are blue\nSugar is sweet\nBut I have commitment issues\nSo I'd rather just be friends\nAt this point in our relationship.")
